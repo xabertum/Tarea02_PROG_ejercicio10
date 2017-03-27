07cef2bd74af6aaeb68f4aaeb83e1d2fcdf7a7d0
xabertum <mohatar@gmail.com>
2017-03-27 12:12:30 +0200

    first commit

---
+++ src/tarea02_ejercicio10/Tarea02_Ejercicio10.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea02_ejercicio10;
+
+/**
+ *
+ * @author mohat
+ */
+public class Tarea02_Ejercicio10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        float x, y;
+        int i,j,z;
+        double dx, dz;
+        byte bx, by, bz;
+        short sx, sy, sz;
+        char cx, xy, cz;
+        
+        System.out.println("----- Conversiones entre enteros y coma flotante -----");
+        System.out.println("Producto de int por float: ");
+        
+        
+        
+    }
+    
+}
