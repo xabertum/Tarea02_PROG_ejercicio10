dccb2ad0611764de2e7a37da57a6319e6b9741d1
xabertum <mohatar@gmail.com>
2017-03-27 13:00:57 +0200

    first changes

--- src/tarea02_ejercicio10/Tarea02_Ejercicio10.java
+++ src/tarea02_ejercicio10/Tarea02_Ejercicio10.java
@@ -21,11 +21,26 @@ public class Tarea02_Ejercicio10 {
         int i = 2,  j = (int) (i * x);
         double dx = 2.0, dz = dx * y;
         byte bx = 5, by = 2, bz = (byte) (bx - by);
-        short sx = 5, sy = 2, sz = (short) (sx - sy);
+        short sx = 5, sy = 2, sz = (short) (sx * sy);
         char cx = '\u000F', cy = '\u0001';
         
         System.out.println("----- Conversiones entre enteros y coma flotante -----");
-        System.out.println("Producto de int por float: j = i*x =  " + j );
+        System.out.println("Producto de int por float: j = i * x = " + j );
+        System.out.println("Producto de float por double: dz = dx * y = " + dz);
+        
+        System.out.println("----- Operaciones con byte -----");
+        System.out.println("byte: bx - by = " + bz);
+        
+        bx = -128; by = 1;
+        System.out.println("byte: bx - by = " + bz);
+        
+        System.out.println("----- Operaciones con short -----");
+        System.out.println("short: sx * sy = " + (sz) + "- 1" + "= 3");
+        sx = 32767;
+        System.out.println("short: " + sx + "1 = " + (sx + 1));
+        
+        System.out.println("------ Operaciones con char -----");
+        
         
     }
     
